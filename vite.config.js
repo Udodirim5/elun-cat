47bd8e1ddfdd6eef1e6cf6fb316af0b9c1b4e8ef
udodirim <udodirimwisdom@gmail.com>
2025-03-08 11:08:44 +0100

    Set base path for production deployment in Vite configuration

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,10 @@
 
-import process from 'process';
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-const isProduction = process.env.NODE_ENV === 'production';
-const basename = isProduction ? "/elun-cat/" : "/";
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: basename,
+  base: "/elun-cat/", 
 });

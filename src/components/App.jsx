a1e8db883cab4a780bfba385c74605ad592f9f42
udodirim <udodirimwisdom@gmail.com>
2025-03-10 13:52:06 +0100

    Set base path for routing in App component

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -7,7 +7,7 @@ const App = () => {
 
   return (
     <>
-      <BrowserRouter basename="">
+      <BrowserRouter basename="/">
         <Routes>
           <Route path="/" element={<HomePage />} />
           <Route path="*" element={<ErrorPage />} />

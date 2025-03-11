70995895a67557f6f309ffb4e58e821dcc99306a
udodirim <udodirimwisdom@gmail.com>
2025-03-11 22:06:28 +0100

    Update contract address in data.js

--- data/data.js
+++ data/data.js
@@ -1,4 +1,4 @@
-export const contractAddress = "xxxxxxxx";
+export const contractAddress = "D3RPaiSxzVUmUkWSW1R8ar9wrSBXL1xXxfjB6SPY187X";
 export const launchDate = new Date("2025-03-12T12:00:00Z").getTime(); // Set launch date to 12 PM UTC
 
 const navLinks = [
